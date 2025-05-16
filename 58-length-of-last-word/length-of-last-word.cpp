f359fd8fe624dda6d0f7ce8032089dd28048b033
Sri-280 <srinika280@gmail.com>
2025-05-17 00:02:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (9.59%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         int length=0, n=s.size()-1;
-        while(n>=0 && s[n]==' '){
+        while(/*n>=0&& */s[n]==' '){
             n--;
         }
         for(int i=n;i>=0;i--){
@@ -15,3 +15,5 @@ public:
         return length;
     }
 };
+
+
