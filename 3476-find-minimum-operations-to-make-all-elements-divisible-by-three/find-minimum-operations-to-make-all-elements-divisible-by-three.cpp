3121f6bbaf44f7d9b9514d6450e71db5461964e0
Sri-280 <srinika280@gmail.com>
2025-05-19 00:25:23 +0530

    Time: 0 ms (100.00%) | Memory: 23.2 MB (77.47%) - LeetSync

--- 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
@@ -4,11 +4,7 @@ public:
         int count=0;
         for(auto s:nums){
             if(s%3!=0){
-                /*if(s%3==2){
                     count++;
-                }else{*/
-                    count++;
-                
             }
         }
         return count;
