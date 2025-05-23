08ec2f5f854a0b531347b9d6235e583796a0e6f9
Sri-280 <srinika280@gmail.com>
2025-05-23 15:13:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (38.52%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
         vector<int> res;
-        if(s1==s2) return true;
-        for(int i=0;i<s1.size();i++){
-            if(s1[i]!=s2[i]){
-                res.push_back(i);
-           }
-        }
+        if (s1 == s2)
+            return true;
 
-           if(res.size()!=2) return false;
-           int i=res[0], j=res[1];
-           return (s1[i]==s2[j] && s1[j]==s2[i] );
+        for (int i = 0; i < s1.size(); i++)
+            if (s1[i] != s2[i])
+                res.push_back(i);
+
+        if (res.size() != 2)
+            return false;
+
+        int i = res[0], j = res[1];
+        return (s1[i] == s2[j] && s1[j] == s2[i]);
     }
 };
